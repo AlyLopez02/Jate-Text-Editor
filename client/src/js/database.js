40c4cb6043262e036a24c952674f4acb40ebb20e
alylopez02 <alylopez0217@gmail.com>
2022-07-19 13:41:47 -0400

    filled out database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -16,6 +16,19 @@ const initdb = async () =>
 export const putDb = async (content) => {
   // console.error('putDb not implemented')
 
+  console.log('PUT to the database');
+
+  const jateDb = await openDB('jate', 1);
+
+  const tx = jateDb.transaction('jate', 'readwrite');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.add({ content: content });
+
+  const result = await request;
+  console.log('data saved to database', result);
+  return result;
   
 };
 
@@ -23,7 +36,20 @@ export const putDb = async (content) => {
 export const getDb = async () => {
   // console.error('getDb not implemented')
 
-  
+  console.log('GET from the database');
+
+  const jateDb = await openDB('jate', 1);
+
+  const tx = jateDb.transaction('jate', 'readonly');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.getAll();
+
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+
 };
 
 initdb();
