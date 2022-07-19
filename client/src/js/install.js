564c069ccd900dc78d4e1f1ce45744a59f093815
alylopez02 <alylopez0217@gmail.com>
2022-07-19 13:05:14 -0400

    filled out package.json

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -4,7 +4,8 @@ const butInstall = document.getElementById('buttonInstall');
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
 
-   
+
+
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
